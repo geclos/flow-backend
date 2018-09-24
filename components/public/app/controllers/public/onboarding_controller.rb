ad90774d213819ea3118734efca78aecd0f78831
Gerard Clos <clos.gerard@gmail.com>
2018-09-24 20:06:01 +0200

    added app domain to env variables

--- components/public/app/controllers/public/onboarding_controller.rb
+++ components/public/app/controllers/public/onboarding_controller.rb
@@ -5,7 +5,7 @@ module Public
 
     def create_signup
       signup = SignupInteractor::Create.new(
-        params: signup_params.merge(host: request.host)
+        params: signup_params.merge(host: ENV['APP_DOMAIN'])
       ).call
 
       return redirect_to confirm_url(email: signup.email) if signup.valid?

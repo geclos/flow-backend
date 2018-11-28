eba07012dcd4ccb49bb25791340da3afdf5854da
Gerard Clos <clos.gerard@gmail.com>
2018-11-28 20:39:28 +0000

    ocd

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  default_url_options protocol: :https unless Rails.env.development?
+  unless Rails.env.development?
+    default_url_options protocol: :https
+  end
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
